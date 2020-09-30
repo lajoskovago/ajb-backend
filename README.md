02cfa682c079f78e76bf3655896ae5e8fb3deda8
Sergiu Muresan <sergiu.muresan.official@gmail.com>
2020-09-30 16:11:08 +0300

    Added test controller/model/route. Added information in the README file. Removed other useless stuff from main.js. Added body-parser

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ajb-backend
 Backend for AJB Project
+
+# Structure
+All components go in their own folder src/<component-name>
+
+In it we have 3 types of file:
+ * model: Where we define the object's structure in the database
+ * route: Where we which api path goes to which function
+ * controller: Where we define all functions (that we routed to)
--- src/main.js
+++ src/main.js
@@ -2,23 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const mongoose = require('mongoose');
+const { testRouter } = require('./test/test-route');
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!' + (17 * 5));
-});
-
-app.get('/blog-post', (req, res) => {
-    res.send([{
-        name: 'Blog post 1',
-        // ...
-    }]);
-});
+app.use('/api/test', testRouter);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
-
 
 mongoose.connect('mongodb+srv://Sergiu:123@cluster0.6pyam.mongodb.net/ajb-backend?retryWrites=true&w=majority', {useNewUrlParser: true});
 mongoose.connection.on('error', (err) => {
