29474902fe038aa08ff45eea2892522721e38f2c
Robert Padurariu <robert.padurariu@emanuel.ro>
2020-10-16 17:09:54 +0300

    Update in club-route

--- src/club/club-route.js
+++ src/club/club-route.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
-const { createAll, deleteAll } = require("./club-controller");
+const { findAll, createAll, UpdateClub, deleteAll } = require("./club-controller");
 const clubRouter = Router();
 
 clubRouter.get('/list', findAll);
 clubRouter.post("/", createAll);
+clubRouter.put('/update',UpdateClub);
 clubRouter.delete("/:id", deleteAll);
 
 exports.clubRouter=clubRouter;
