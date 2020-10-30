eae63977306e9d1d749dc4320ed2c5e82d774cbd
Gergo Barcsa <barcsathemiddleman@gmail.com>
2020-10-30 16:21:45 +0200

    up2date

--- src/commission/commission-route.js
+++ src/commission/commission-route.js
@@ -1,8 +1,8 @@
 const { Router } = require("express");
-const { findAll, updateCommission, create, remove} = require("./commission-controller");
+const { findAll, updateCommission, remove, createAll} = require("./commission-controller");
 const commissionRouter = Router();
 
-commissionRouter.post('/create', create);
+commissionRouter.post('/create', createAll);
 commissionRouter.get('/list', findAll);
 commissionRouter.put('/update',updateCommission);
 commissionRouter.delete('/delete', remove);
